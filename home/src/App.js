65aae3da168d94a412a7e76f164cb2e81abf9a1d
MuMeiCC <enderwizard233@gmail.com>
2025-03-20 21:05:55 -0700

    Finished the menu setup

--- home/src/App.js
+++ home/src/App.js
@@ -1,35 +1,17 @@
 import React, {useState} from 'react';
-import './assets/style/App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+import './style/App.css';
+import Menu from './components/Menu.js';
 
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+const App = () => {
 
   return (
-    <div className="App">
-
-      <div className={`menu-button ${isMenuOpen ? 'open':''}`} onClick={toggleMenu}>
-        <div className="menu-bar bar1"></div>
-        <div className="menu-bar bar2"></div>
-        <div className="menu-frame"></div>
+    <Router>
+      <div className='App'>
+        <Menu />
       </div>
-
-      <div className={`menu ${isMenuOpen ? 'open':''}`}>
-        <ul className="menu-list">
-          <li className="menu-item active">Top</li>
-          <li className="menu-item">About</li>
-          <li className="menu-item">Portfolio</li>
-          <li className="menu-item">Skills</li>
-          <li className="menu-item">Blog</li>
-          <li className="menu-item">Contact</li>
-        </ul>
-      </div>
-
-    </div>
+    </Router>
   );
 }
 
---
+++ home/src/components/Menu.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import '../style/Menu.css';
+
+const Menu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+  const toggleMenu = () => {
+	setIsMenuOpen(!isMenuOpen);
+  }
+
+  return (
+	<div className="menu-container">
+
+      <div className={`menu-button ${isMenuOpen ? 'open':''}`} onClick={toggleMenu}>
+        <div className="menu-bar bar1"></div>
+        <div className="menu-bar bar2"></div>
+        <div className="menu-frame"></div>
+      </div>
+
+      <div className={`menu ${isMenuOpen ? 'open':''}`}>
+        <nav className="menu-list">
+          <NavLink to='/' className='menu-item' activeClassName='active'>Top</NavLink>
+          <NavLink to='/about' className='menu-item' activeClassName='active'>About</NavLink>
+          <NavLink to='/portfolio' className='menu-item' activeClassName='active'>Portfolio</NavLink>
+          <NavLink to='/skills' className='menu-item' activeClassName='active'>Skills</NavLink>
+          <NavLink to='/blog' className='menu-item' activeClassName='active'>Blog</NavLink>
+          <NavLink to='/contact' className='menu-item' activeClassName='active'>Contact</NavLink>
+        </nav>
+      </div>
+
+    </div>
+  )
+}
+
+export default Menu;
